2271e126a814f7584b259195fbb982bdc0f93618
Adriana Montes <adriana.montesmag@gmail.com>
2021-06-16 10:41:17 -0500

    Segundo video

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -2,20 +2,20 @@ package com.platzi.javatests.util;
 
 public class StringUtilTest {
     public static void main(String[] args) {
-        String result = StringUtil.repeat("hola", 3);
+        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
 
-        if (!result.equals("holaholahola")){
-            System.out.println("ERROR");
+
+        assertEquals(StringUtil.repeat("hola", 1), "hola");
+
+
+
+
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + "is not equal to expected" + expected);
         }
-
-        String result2 = StringUtil.repeat("hola", 1);
-
-        if (!result2.equals("hola")){
-            System.out.println("ERROR");
-        }
-
-
-
     }
 
 }
